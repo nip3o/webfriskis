fde87a7f24e04bff247e310c025d02c5eea03aaf
Niclas Olofsson <n@niclasolofsson.se>
2015-03-26 17:12:39 +0100

    Working date navigation and filtering.

--- static/activity-list/activity-list-view.js
+++ static/activity-list/activity-list-view.js
@@ -1,20 +1,45 @@
 'use strict';
 
 angular.module('FriskisApp').controller('ActivityListViewController', function ($scope, $http, $mdBottomSheet, Activity) {
-    $scope.dates = [new Date(2015, 1, 15), new Date(2015, 1, 16), new Date(2015, 1, 17), new Date(2015, 1, 18)];
+    var activitiesByDate = {};
+
+    function getDateKey(date) {
+        return moment(date).format('YYYY-MM-DD')
+    }
+
+    function createDateSequence(length) {
+        var date = moment();
+        var result = []
+
+        for (var i = 0; i < length; i++) {
+            result.push(date.clone().toDate());
+            date.add(1, 'days');
+        };
+
+        return result;
+    }
+
+    $scope.dates = createDateSequence(5);
+    console.log($scope.dates);
 
     $http.get('/activities').then(function (response) {
-        $scope.activities = response.data.result.map(function (activity) {
+        response.data.result.forEach(function (activity) {
             activity.start = new Date(activity.start_dt);
             activity.end = new Date(activity.end_dt);
 
-            return activity;
+            var key = getDateKey(activity.start_dt);
+
+            if (!activitiesByDate[key]) {
+                activitiesByDate[key] = [];
+            }
+            activitiesByDate[key].push(activity);
         });
+        $scope.activitiesByDate = activitiesByDate;
     })
 
-    // Activity.query(function (activities) {
-    //     $scope.activities = activities;
-    // });
+    $scope.getActivities = function (date) {
+        return $scope.activitiesByDate[getDateKey(date)];
+    }
 
 
 }).controller('ActivitySheetController', function($scope, $mdBottomSheet, activity) {
